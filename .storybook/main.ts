bf577b18dfb3a898a21e288ce99eb77b17b63a81
Maria Bogdanova <masha.bogdanoff@gmail.com>
2025-03-07 15:57:10 +0100

    chore: add storybook documentation

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,21 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
-import * as path from "path";
+import * as path from 'path';
 
 const config: StorybookConfig = {
-  stories: [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions"
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/react-webpack5",
-    options: {}
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    defaultName: 'Documentation',
   },
   webpackFinal: async (config) => {
     config.resolve.alias = {
@@ -29,20 +29,20 @@ const config: StorybookConfig = {
       {
         test: /\.module\.s[ac]ss$/i,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               modules: true,
             },
           },
-          "sass-loader",
+          'sass-loader',
         ],
       },
       {
         test: /\.s[ac]ss$/i,
         exclude: /\.module\.s[ac]ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       }
     );
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Button } from './components/Button/Button';
 export { default as Checkbox } from './components/Checkbox/Checkbox';
 export { default as Select } from './components/Select/Select';
-export { default as TextField } from './components/TextField/TextField';
 export { default as Switch } from './components/Switch/Switch';
+export { default as TextField } from './components/TextField/TextField';
